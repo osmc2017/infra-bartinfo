c578fb5a4d9e643290d5f335263df633811d9c50
osmc2017 <bartairelionel@gmail.com>
2024-12-24 10:26:32 +0100

    new

--- ADDC/Utilisateurs/script_users.ps1
+++ ADDC/Utilisateurs/script_users.ps1
@@ -18,6 +18,12 @@ $data = Import-Csv -Path $csvPath
 # Mot de passe par défaut pour tous les utilisateurs
 $defaultPassword = ConvertTo-SecureString "Azerty1*" -AsPlainText -Force
 
+# Fonction pour nettoyer les noms et supprimer les caractères spéciaux
+function CleanName {
+    param ([string]$name)
+    return $name -replace "[^a-zA-Z0-9]", ""
+}
+
 # Parcourir chaque ligne du fichier CSV pour créer les utilisateurs
 foreach ($user in $data) {
     # Récupérer les informations utilisateur depuis le CSV
@@ -25,15 +31,21 @@ foreach ($user in $data) {
     $lastName = $user.Nom
     $department = $user.Département
     $service = $user.Service
-    $samAccountName = "$($firstName.Substring(0,1).ToLower())$($lastName.ToLower())"
     $telephoneFixe = $user."Téléphone fixe"
     $telephonePortable = $user."Téléphone portable"
 
+    # Nettoyer les noms pour le SamAccountName et les DN
+    $cleanFirstName = CleanName $firstName
+    $cleanLastName = CleanName $lastName
+    $samAccountName = "$($cleanFirstName.Substring(0,1).ToLower())$($cleanLastName.ToLower())"
+    $cleanDepartment = CleanName $department
+    $cleanService = if (-Not [string]::IsNullOrWhiteSpace($service) -and $service -ne "-") { CleanName $service } else { "" }
+
     # Construire le Distinguished Name (DN) pour placer l'utilisateur dans la bonne OU
-    if (-Not [string]::IsNullOrWhiteSpace($service) -and $service -ne "-") {
-        $ouPath = "OU=$service,OU=$department,OU=Departements,DC=demo,DC=lan"
+    if (-Not [string]::IsNullOrWhiteSpace($cleanService)) {
+        $ouPath = "OU=$cleanService,OU=$cleanDepartment,OU=Departements,DC=demo,DC=lan"
     } else {
-        $ouPath = "OU=$department,OU=Departements,DC=demo,DC=lan"
+        $ouPath = "OU=$cleanDepartment,OU=Departements,DC=demo,DC=lan"
     }
 
     # Vérifier si l'utilisateur existe déjà
