9adc32ce7e46073c9310e214d2856bf5761d198f
osmc2017 <bartairelionel@gmail.com>
2024-12-26 11:20:09 +0100

    new

--- ADDC/GROUPE/script_group.ps1
+++ ADDC/GROUPE/script_group.ps1
@@ -4,32 +4,40 @@ Import-Module ActiveDirectory
 # OU principale contenant les départements
 $rootOU = "OU=Départements,DC=test,DC=lan"  # Remplace par ton chemin exact
 
-# Parcours des départements
-$departements = Get-ADOrganizationalUnit -Filter * -SearchBase $rootOU
+# Fonction pour vérifier si un groupe existe
+function GroupExists {
+    param (
+        [string]$groupName
+    )
+    return (Get-ADGroup -Filter "Name -eq '$groupName'" -ErrorAction SilentlyContinue)
+}
+
+# Parcours des départements (OUs enfants directs de l'OU root)
+$departements = Get-ADOrganizationalUnit -Filter * -SearchBase $rootOU | Where-Object { $_.DistinguishedName -notlike "*OU=Départements*" }
 
 foreach ($departement in $departements) {
     $deptName = $departement.Name
     $deptPath = $departement.DistinguishedName
 
-    # Crée un groupe pour le département
+    # Crée un groupe pour le département seulement si c'est un niveau pertinent
     $deptGroupName = "G_Departement_$deptName"
-    if (-not (Get-ADGroup -Filter {Name -eq $deptGroupName})) {
+    if (-not (GroupExists $deptGroupName)) {
         New-ADGroup -Name $deptGroupName -GroupScope Global -GroupCategory Security -Path $deptPath -Description "Groupe pour le département $deptName"
         Write-Host "Groupe créé : $deptGroupName"
     } else {
         Write-Host "Le groupe $deptGroupName existe déjà."
     }
 
-    # Parcours des services dans le département (uniquement les OUs enfants directs)
+    # Parcours des services dans ce département (enfants directs uniquement)
     $services = Get-ADOrganizationalUnit -Filter * -SearchBase $deptPath | Where-Object { $_.DistinguishedName -notlike "*OU=Départements*" }
 
     foreach ($service in $services) {
         $serviceName = $service.Name
         $servicePath = $service.DistinguishedName
 
-        # Crée un groupe pour le service
+        # Crée un groupe pour le service uniquement
         $serviceGroupName = "G_Service_$serviceName"
-        if (-not (Get-ADGroup -Filter {Name -eq $serviceGroupName})) {
+        if (-not (GroupExists $serviceGroupName)) {
             New-ADGroup -Name $serviceGroupName -GroupScope Global -GroupCategory Security -Path $servicePath -Description "Groupe pour le service $serviceName"
             Write-Host "Groupe créé : $serviceGroupName"
         } else {
