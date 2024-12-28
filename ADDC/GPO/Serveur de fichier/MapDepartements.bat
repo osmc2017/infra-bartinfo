9ad361f2a225f18f9bf6ffa2154a2b8c65087a5a
osmc2017 <bartairelionel@gmail.com>
2024-12-28 10:03:06 +0100

    new

--- ADDC/GPO/Serveur de fichier/MapDepartements.bat
+++ ADDC/GPO/Serveur de fichier/MapDepartements.bat
@@ -1,22 +1,24 @@
 @echo off
+set LOGFILE=C:\MapDriveLog.txt
+echo Début du script de mapping des lecteurs réseau >> %LOGFILE%
 
-:: Variables
+:: Serveur et chemin de base
 set "Server=\\SRV_FICHIER"
-set "BasePath=Departements"
+set "BasePath=Partage\Departements"
 
-:: Obtenir l'utilisateur connecté
+:: Obtenir le nom de l'utilisateur connecté
 for /f "tokens=2 delims==" %%i in ('whoami /upn') do set "User=%%i"
 
-:: Extraire le département à partir de l'UPN ou DN (remplacez selon votre logique)
+:: Extraire le département (remplacez cette logique selon vos besoins)
 for /f "tokens=2 delims=@" %%i in ("%User%") do set "Department=%%i"
+echo Utilisateur : %User%, Département : %Department% >> %LOGFILE%
 
-:: Mapper le lecteur réseau
-echo Mapping drive for department: %Department%
-net use K: %Server%\%BasePath%\%Department% /persistent:no
+:: Mapper le lecteur réseau pour le département
+echo Tentative de mapping du lecteur réseau : %Server%\%BasePath%\%Department% >> %LOGFILE%
+net use Z: %Server%\%BasePath%\%Department% /persistent:no >> %LOGFILE% 2>&1
 
 if errorlevel 1 (
-    echo Échec du mapping du lecteur réseau.
+    echo Échec du mapping du lecteur réseau pour %Department%. >> %LOGFILE%
 ) else (
-    echo Lecteur réseau mappé avec succès.
+    echo Lecteur réseau mappé avec succès pour %Department%. >> %LOGFILE%
 )
-pause
